205152eab787c16dc0d425c977c7465bc27870fd
nasa14119 <nico.amaya0205@gmail.com>
2025-11-24 07:34:49 -0600

    feat(Registers): changing some minor aspects

--- components/ErrorRegisters.tsx
+++ components/ErrorRegisters.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 type Props = { status: number };
 export function ErrorRegisters({ status }: Props) {
   return (
-    <div className="w-dvw h-dvh flex justify-center items-center mx-auto">
+    <div className="w-dvw h-dvh flex justify-center items-start mx-auto max-w-[500px] pt-2">
       <div className="rounded-4xl w-[90%] h-[90%] flex justify-center items-center bg-neutral-300/20 flex-col">
         {status === 204 ? (
           <RiDatabase2Fill className="size-30 text-black/90" />
@@ -12,11 +12,11 @@ export function ErrorRegisters({ status }: Props) {
           <RiCloudOffFill className="size-30 text-black/90" />
         )}
         <span className="font-semibold text-2xl">
-          {status === 204 ? "Database empty nothing to see" : "Server down"}
+          {status === 204 ? "Database empty" : "Server down"}
         </span>
         <Link
           href="/"
-          className="bg-neutral-300 text-blue-950 w-3/4 py-1 text-xs text-center rounded-3xl font-semibold mt-2"
+          className="bg-neutral-300 text-blue-950 w-3/4 py-1 text-xs text-center rounded-3xl font-semibold mt-2 max-w-[200px]"
         >
           Go Home
         </Link>
