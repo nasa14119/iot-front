dee76ab6af6655d8b70ab009656183ac3a1eeda2
nasa14119 <nico.amaya0205@gmail.com>
2025-12-02 08:04:21 -0600

    fix(registers): error in nav var fix

--- components/ChartDisplay/index.tsx
+++ components/ChartDisplay/index.tsx
@@ -18,8 +18,8 @@ function Reset() {
   const today = new Date();
   if (format(today, "DD/MM/YY", "es") !== format_date)
     return (
-      <div className="sticky top-12 inset-x-0 fade-in  z-50 sm:-mt-8 sm:top-2">
-        <div className="max-w-[500px] mx-auto flex justify-start sm:justify-end px-2 pointer-events-none">
+      <div className="sticky top-12 inset-x-0 fade-in  z-50 sm:-mt-8 sm:top-2 pointer-events-none">
+        <div className="max-w-[500px] mx-auto flex justify-start sm:justify-end px-2 ">
           <button
             className="rounded-4xl px-2 py-1 bg-neutral-200 pointer-events-auto font-semibold text-black/80"
             onClick={reset}
